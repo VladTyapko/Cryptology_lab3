d38e2f6ee689d965efbcc0d5187634853c9c57c1
vladislav <vladtiapko@gmail.com>
2021-06-07 20:30:55 +0300

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        byte[] arr = Ripemd160.getHash("".getBytes());
+        System.out.println(Ripemd160.bytesToHex(arr));
+    }
+}
